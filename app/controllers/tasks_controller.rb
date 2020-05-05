58efef0d7eb90b959f7e968e9eabea3ca20c1b9c
m.b <m.9745839.b@gmail.com>
2020-05-05 01:11:38 +0000

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-	before_action :correct_user, only: [:destroy]
+	before_action :correct_user, only: [:show, :edit, :update, :destroy]
 	
 	def index
 	  @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
@@ -12,7 +11,7 @@ class TasksController < ApplicationController
   end
 
   def new
-  	@task =Task.new
+  	@task = Task.new
   end
 
   def create
@@ -51,10 +50,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only:[:index]
-  
-  def index
-  end
   	
 	def new
 		@user = User.new
